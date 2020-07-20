3671f2ce28465cea6384cd199d0a6d66c89ee831
Renan Oliveira <renan04lima@outlook.com>
2020-07-20 20:24:22 -0300

    Exceções

---
+++ POO, exceções e bancos de dados/exceções/3.php
@@ -0,0 +1,20 @@
+<?php
+
+
+function validarUsuario(array $usuario)
+{
+    if (empty($usuario['codigo']) || empty($usuario['nome']) || empty($usuario['idade'])) {
+        throw new Exception("Campos obrigatórios não foram preenchidos!");
+    }
+
+    return true;
+}
+
+$usuario = array(
+    'codigo' => 1,
+    'nome' => '',
+    'idade' => 57,
+);
+
+validarUsuario($usuario);
+
